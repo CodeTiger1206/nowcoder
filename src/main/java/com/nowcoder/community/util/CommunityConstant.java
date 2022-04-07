afc4c352d7217739b570a2c8aa16e267b82160b7
cabuda <1226815246@qq.com>
2022-04-07 19:58:06 +0800

    开发注册功能

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.util;
+
+/**
+ * @Program: community
+ * @Auther: ldh
+ * @Date: 2022/4/7
+ * @Description:
+ */
+public interface CommunityConstant {
+    //激活成功
+    int ACTVIATION_SUCCESS = 0;
+
+    //激活失败
+    int ACTVIATION_FAILURE = 2;
+
+    //重复激活
+    int ACTVIATION_REPEAT = 1;
+}
